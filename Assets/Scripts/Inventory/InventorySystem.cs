99216de622cdf1d5c17ad0b91b7635d4e326b553
Thedonpasco <114503573+Thedonpasco@users.noreply.github.com>
2022-12-12 09:25:50 +0000

    Work on inventory System

---
+++ Assets/Scripts/Inventory/InventorySystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+
+[System.Serializable]
+
+public class InventorySystem 
+{
+
+    [SerializeField] private List<InventorySlots> inventorySlots;
+
+    public List<InventorySlots> InventorySlots => inventorySlots;
+
+    public int InventorySize => InventorySlots.Count;
+
+    public UnityAction<InventorySlots> OnInventorySlotsChanged;
+
+
+    public InventorySystem(int size)
+    {
+        inventorySlots = new List<InventorySlots>(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            inventorySlots.Add(new InventorySlots());
+        }
+
+
+    }
+
+}
